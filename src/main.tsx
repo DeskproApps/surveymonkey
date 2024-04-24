96d429b9eb940a353f7bfe151d7a49aba381c07d
ITweeno <itweeno@gmail.com>
2024-04-24 14:36:35 +0100

    add iframe resizer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { query } from "./utils/query";
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
