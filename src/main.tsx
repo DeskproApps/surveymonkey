4d05acc056d95ac135bb5b9b9b2b3b89264cd4bf
ilia makarov <ilia.makarov@me.com>
2024-06-04 17:13:03 +0300

    Removed iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { query } from "./utils/query";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
