f63cfa391c4906d34de4207d1aca618eef693a1a
OJ Abba <phavorabba@gmail.com>
2025-03-14 16:00:12 +0000

    [Bug] Fix runaway polling bug in useLogin (SC-183520)

--- src/pages/Login/useLogin.ts
+++ src/pages/Login/useLogin.ts
@@ -2,7 +2,7 @@ import { ContextData, Settings } from "@/types/deskpro";
 import { createSearchParams, useNavigate } from "react-router-dom";
 import { getAccessToken, getCurrentUser } from "@/api/SurveyMonkey";
 import { OAUTH2_ACCESS_TOKEN_PATH, OAUTH2_REFRESH_TOKEN_PATH } from "@/constants/deskpro";
-import { OAuth2Result, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
+import { IOAuth2, OAuth2Result, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
 import { useCallback, useState } from "react";
 
 interface UseLogin {
@@ -16,12 +16,14 @@ export default function useLogin(): UseLogin {
     const [authUrl, setAuthUrl] = useState<string | null>(null)
     const [error, setError] = useState<null | string>(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [isPolling, setIsPolling] = useState(false)
+    const [oauth2Context, setOAuth2Context] = useState<IOAuth2 | null>(null)
+
     const navigate = useNavigate()
 
     const { context } = useDeskproLatestAppContext<ContextData, Settings>()
 
     const user = context?.data?.ticket?.primaryUser || context?.data?.user
-
 
     useInitialisedDeskproAppClient(async (client) => {
         if (context?.settings.use_deskpro_saas === undefined || !user) {
@@ -33,30 +35,31 @@ export default function useLogin(): UseLogin {
         if (context.settings.use_access_token === true) {
             setError("Enable OAuth to access this page");
             return
-
         }
         const mode = context?.settings.use_deskpro_saas ? 'global' : 'local';
 
         const clientId = context?.settings.client_id;
-        if (mode === 'local' && typeof clientId !== 'string') {
+        if (mode === 'local' && (typeof clientId !== 'string' || clientId.trim() === "")) {
             // Local mode requires a clientId.
             setError("A client ID is required");
             return
         }
-        const oauth2 = mode === "local" ?
+
+        // Start OAuth process depending on the authentication mode
+        const oauth2Response = mode === "local" ?
             await client.startOauth2Local(
                 ({ state, callbackUrl }) => {
                     return `https://api.surveymonkey.com/oauth/authorize?${createSearchParams([
                         ["client_id", clientId ?? ""],
                         ["state", state],
                         ["redirect_uri", callbackUrl],
-                        ["response_type", "code"] 
+                        ["response_type", "code"]
                     ])}`;
                 },
                 /\bcode=(?<code>[^&#]+)/,
                 async (code: string): Promise<OAuth2Result> => {
                     // Extract the callback URL from the authorization URL
-                    const url = new URL(oauth2.authorizationUrl);
+                    const url = new URL(oauth2Response.authorizationUrl);
                     const redirectUri = url.searchParams.get("redirect_uri");
 
                     if (!redirectUri) {
@@ -71,36 +74,51 @@ export default function useLogin(): UseLogin {
             // Global Proxy Service
             : await client.startOauth2Global("TW2mwcHyQwCmkrzNjgdMAQ");
 
-        setAuthUrl(oauth2.authorizationUrl)
-        setIsLoading(false)
+        setAuthUrl(oauth2Response.authorizationUrl)
+        setOAuth2Context(oauth2Response)
 
-        try {
-            const result = await oauth2.poll()
-
-            await client.setUserState(OAUTH2_ACCESS_TOKEN_PATH, result.data.access_token, { backend: true })
-
-            if (result.data.refresh_token) {
-                await client.setUserState(OAUTH2_REFRESH_TOKEN_PATH, result.data.refresh_token, { backend: true })
-            }
-
-            const activeUser = await getCurrentUser(client)
-
-            if (!activeUser) {
-                throw new Error("Error authenticating user")
-            }
-
-            navigate("/home")
-        } catch (error) {
-            setError(error instanceof Error ? error.message : 'Unknown error');
-            setIsLoading(false);
-        }
     }, [setAuthUrl, context?.settings.use_deskpro_saas])
+
+    useInitialisedDeskproAppClient((client) => {
+        if (!user || !oauth2Context) {
+            return
+        }
+
+        const startPolling = async () => {
+            try {
+                const result = await oauth2Context.poll()
+
+                await client.setUserState(OAUTH2_ACCESS_TOKEN_PATH, result.data.access_token, { backend: true })
+
+                if (result.data.refresh_token) {
+                    await client.setUserState(OAUTH2_REFRESH_TOKEN_PATH, result.data.refresh_token, { backend: true })
+                }
+
+                const activeUser = await getCurrentUser(client)
+
+                if (!activeUser) {
+                    throw new Error("Error authenticating user")
+                }
+
+                navigate("/home")
+            } catch (error) {
+                setError(error instanceof Error ? error.message : 'Unknown error');
+            } finally {
+                setIsLoading(false)
+                setIsPolling(false)
+            }
+        }
+
+        if (isPolling) {
+            void startPolling()
+        }
+    }, [isPolling, user, oauth2Context, navigate])
 
     const onSignIn = useCallback(() => {
         setIsLoading(true);
+        setIsPolling(true);
         window.open(authUrl ?? "", '_blank');
     }, [setIsLoading, authUrl]);
-
 
     return { authUrl, onSignIn, error, isLoading }
 
